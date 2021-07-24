b35ea144cf19a82eaec9b0195824293f82d03532
Linlin <denglinlin315@gmail.com>
2021-07-24 21:53:52 +1000

    name sorter

---
+++ name-sorter-app/Person.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace name_sorter_app
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+
+        public override string ToString()
+        {
+            return (MiddleName != "") ? FirstName + " " + MiddleName + " " + LastName
+                                        : FirstName + " " + LastName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person)
+            {
+                var other = obj as Person;
+
+                return FirstName == other.FirstName &&
+                    LastName == other.LastName &&
+                    MiddleName == other.MiddleName;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return FirstName.GetHashCode() + LastName.GetHashCode() + MiddleName.GetHashCode();
+        }
+
+        public static int ComparePeople(Person p1, Person p2)
+        {
+            if (p1.Equals(p2))
+            {
+                return 0;
+            }
+
+            if (p1.LastName.CompareTo(p2.LastName) == 0)
+            {
+                if (p1.FirstName.CompareTo(p2.FirstName) == 0)
+                {
+                    return p1.MiddleName.CompareTo(p2.MiddleName);
+                }
+                else
+                {
+                    return p1.FirstName.CompareTo(p2.FirstName);
+                }
+            }
+            else
+            {
+                return p1.LastName.CompareTo(p2.LastName);
+            }
+
+        }
+
+        public static void SortPeople(Person [] people)
+        {
+            if (people.Length <= 1)
+            {
+                return;
+            }
+
+            for (var i = 0; i < people.Length - 1; i++)
+            {
+                var minI = i;
+                for (var j = i + 1; j < people.Length; j++)
+                {
+                    if (ComparePeople(people[minI], people[j]) > 0)
+                    {
+                        minI = j;
+                    }
+                }
+                Person swapped = people[minI];
+                people[minI] = people[i];
+                people[i] = swapped;
+            }
+        }
+
+
+ 
+    }
+}
--- name-sorter-app/Program.cs
+++ name-sorter-app/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace name_sorter_app
 {
@@ -6,7 +7,71 @@ namespace name_sorter_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! " + args.Length);
+
+            if (args.Length != 1)
+            {
+               throw new Exception("Usage: name-sorter-app fileName");
+            }
+
+            var contents = File.ReadAllText(args[0]);
+            var names = contents.Split('\n');
+            var people = new Person[names.Length];
+            var added = 0;
+            foreach (var name in names)
+            {
+                var subnames = name.Split(' ');
+                Person person;
+                if (subnames.Length == 1)
+                {
+                    person = new Person
+                    {
+                        LastName = subnames[0],
+                        FirstName = "",
+                        MiddleName = ""
+                    };
+                }
+                else if (subnames.Length == 2)
+                {
+                    person = new Person
+                    {
+                        FirstName = subnames[0],
+                        LastName = subnames[1],
+                        MiddleName = ""
+                    };
+                }
+                else
+                {
+                    var middlenames = "";
+
+                    var first = subnames[0];
+                    var last = subnames[subnames.Length - 1];
+                    var limit = subnames.Length - 2;
+                    for (var i = 1; i < limit; i++)
+                    {
+                        middlenames += subnames[i];
+                        if (i < limit - 1)
+                        {
+                            middlenames += " ";
+                        }
+                    }
+                    person = new Person
+                    {
+                        FirstName = first,
+                        LastName = last,
+                        MiddleName = middlenames
+                    };
+                }
+                people[added++] = person;
+            }
+
+
+            Person.SortPeople(people);
+
+            foreach (var person in people)
+            {
+                Console.WriteLine(person.ToString());
+            }
         }
     }
 }
